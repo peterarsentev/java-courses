d0649d9b9261187edc75b1e89f9b2cb121db6c2f
Petr Arsentev <arsentyev.petr@otr.ru>
2015-04-03 09:30:21 +0300

    Обновление описания

---
+++ src/main/java/ru/lesson/lessons/UserException.java
@@ -0,0 +1,13 @@
+package ru.lesson.lessons;
+
+
+/**
+ * TODO: comment
+ * @author parsentev
+ * @since 31.03.2015
+ */
+public class UserException extends Exception {
+	public UserException(final String message) {
+		super(message);
+	}
+}
