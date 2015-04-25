581339263e56b19b7ec0f2c9b8ecebb6fec928ec
Petr Arsentev <arsentyev.petr@otr.ru>
2015-04-25 22:58:18 +0300

    #18 SQL

---
+++ clinic-pet-web/sql/schema.sql
@@ -0,0 +1,32 @@
+-- create table
+create table client (
+	uid serial primary key,
+	name varchar(200)
+);
+
+create table pet (
+	uid serial primary key,
+	client_id int not null references client(uid),
+	nick varchar(200)
+);
+
+-- add new client
+insert into client (name) values ('petr');
+
+-- add new pet
+insert into pet (client_id, nick) values (1, 'sparky');
+
+insert into pet (client_id, nick) values (1, 'boby');
+
+-- select pet with client
+select pet.nick, client.name from pet as pet
+inner join client as client on client.uid = pet.client_id;
+
+-- select client by name
+select * from client as client where client.name = 'petr'
+
+-- update pet
+update pet as pet set nick = 'bob' where pet.nick = 'boby'
+
+-- delete pet by nick
+delete from pet as pet where pet.nick = 'bob'
